cfacd9a4a4b69be71030ca5e002d47d2400c09d8
jakranon <jakranonnok_1994@hotmail.com>
2023-09-03 12:28:13 +0700

    commit

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 
 // TODO: ใส่รหัสนักศึกษาที่ค่าสตริงนี้
-const studentId = 'Student ID';
+const studentId = '630710743';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -20,40 +20,121 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          padding: EdgeInsets.symmetric(
-            horizontal: 20.0,
-            vertical: 20.0,
-          ),
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/images/bg_colorful.jpg"),
-              opacity: 0.6,
-              fit: BoxFit.cover,
+            padding: EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 20.0,
             ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text('Good Morning',
-                  textAlign: TextAlign.center, style: textTheme.headlineMedium),
-              Text(studentId,
-                  textAlign: TextAlign.center,
-                  style: textTheme.headlineSmall!.copyWith(
-                      fontWeight: FontWeight.bold, color: Colors.black87)),
-              Spacer(),
-              _buildQuizView(),
-              Spacer(),
-              _buildButtonPanel(),
-              SizedBox(height: 16.0),
-            ],
-          ),
-        ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/bg_colorful.jpg"),
+                opacity: 0.6,
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text('Good Morning',
+                    textAlign: TextAlign.center,
+                    style: textTheme.headlineMedium),
+                Text(studentId,
+                    textAlign: TextAlign.center,
+                    style: textTheme.headlineSmall!.copyWith(
+                        fontWeight: FontWeight.bold, color: Colors.black87)),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: 400,
+                          height: 400,
+                          decoration: ShapeDecoration(
+                            color: Colors.white,
+                            shape: ContinuousRectangleBorder(
+                                borderRadius: BorderRadius.circular(32)),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  height: 20.0,
+                                ),
+                                Text('Question 30 of 30'),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Container(
+                                    width: 300,
+                                    height: 60,
+                                    decoration: ShapeDecoration(
+                                      color: Colors.green,
+                                      shape: ContinuousRectangleBorder(
+                                          borderRadius: BorderRadius.circular(32)),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.center,
+                                        children: [
+                                          SizedBox(height: 20.0,
+                                          ),
+                                          Text('What colors my like'),
+                                          SizedBox(
+                                            height: 20.0,
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Container(
+                                              width: 400,
+                                              height: 30,
+                                              decoration: ShapeDecoration(
+                                                color: Colors.blue[200],
+                                                shape: ContinuousRectangleBorder(
+                                                    borderRadius: BorderRadius.circular(32)),
+                                              ),
+                                              child: Column(
+                                                crossAxisAlignment: CrossAxisAlignment.center,
+                                                children: [
+                                                  SizedBox(
+                                                    height: 20.0,
+                                                  ),
+                                                  Text('Green')
+                                                ],
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Spacer(),
+                _buildQuizView(),
+                Spacer(),
+                _buildButtonPanel(),
+                SizedBox(height: 16.0),
+              ],
+            )),
       ),
     );
   }
 
   _buildQuizView() {
-    // TODO: build UI
+    //
     return Center(child: Text('TODO: build UI'));
   }
 
